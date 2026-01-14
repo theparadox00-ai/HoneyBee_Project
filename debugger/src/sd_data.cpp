b81000efc59b5704ffec2a64f55a8ce49e1e2aec
theparadox00-ai <suryadiya04@gmail.com>
2026-01-14 13:03:44 +0530

    Update sd_data.cpp

--- debugger/src/sd_data.cpp
+++ debugger/src/sd_data.cpp
@@ -1,7 +1,10 @@
+/*This file presents all the neccessary logic to create a data packet for efficient data transmission */
+
 #include "sd_data.h"
 
 const char* DIR_LOADCELL = "/LoadCell";
 const char* DIR_SHT      = "/SHT45";
+const char* DIR_AUDIO = "/ICS-43434";
 
 bool Init_SD() {
     if (!SD.begin(SD_CS_PIN)) {
@@ -9,8 +12,16 @@ bool Init_SD() {
         return false;
     }
     
-    if (!SD.exists(DIR_LOADCELL)) SD.mkdir(DIR_LOADCELL);
-    if (!SD.exists(DIR_SHT)) SD.mkdir(DIR_SHT);
+    if (!SD.exists(DIR_LOADCELL){
+        SD.mkdir(DIR_LOADCELL);
+    }
+    if (!SD.exists(DIR_SHT){ 
+        SD.mkdir(DIR_SHT);
+    }
+    if (!SD.exists(DIR_AUDIO){ 
+        SD.mkdir(DIR_AUDIO);
+    }
+    
     String lcPath = String(DIR_LOADCELL) + "/data.csv";
     if (!SD.exists(lcPath)) {
         File f = SD.open(lcPath, FILE_WRITE);
@@ -30,7 +41,13 @@ bool Init_SD() {
             Serial.println("Created SHT file");
         }
     }
-    
+
+    String audioPath = String(DIR_AUDIO) + "/data.wav";
+    if(!SD.exists(audioPath)){
+        File f = SD.open(audioPath,FILE_WRITE);
+        if(f){
+            /* figure out a way to create the data file here if you can */
+    }
     return true;
 }
 
@@ -64,6 +81,11 @@ void SHT_write(char* time, float temp, float humi) {
     }
 }
 
+void Audio_write(){
+    /*I want to use this function to store data into the SD card i dont want the data to be saved from the audio.cpp file*/
+}
+
 void SD_Sleep() {
     SD.end(); 
 }
+
