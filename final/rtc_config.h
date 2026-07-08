97652b5768f3cfb356279243607d4ee8815a61c1
theparadox00-ai <suryadiya04@gmail.com>
2026-07-08 20:52:41 +0530

    Update rtc_config.h with new RTC functions
    Added new functions for RTC synchronization and sleep management.

--- final/rtc_config.h
+++ final/rtc_config.h
@@ -1,11 +1,21 @@
 #ifndef RTC_CONFIG_H
 #define RTC_CONFIG_H
-
+ 
 #include "config.h"
+#include "sd_card.h"
 
 bool  initRTC();
+ 
 char* Time_(char* buffer);
+ 
 bool  clock_Synchronization();
-void  Alarm_();
+ 
+bool  rtc_needsSync();
 
-#endif
+bool  rtc_syncIfNeeded();
+ 
+void goToSleep(uint32_t sleep_duration);
+
+void foreverSleep();
+ 
+#endif 
