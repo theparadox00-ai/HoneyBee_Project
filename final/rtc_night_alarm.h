1cf660d67462372a5852c206f7613d829feea2d1
theparadox00-ai <suryadiya04@gmail.com>
2026-07-08 20:54:59 +0530

    Update rtc_night_alarm.h

--- final/rtc_night_alarm.h
+++ final/rtc_night_alarm.h
@@ -1,9 +1,26 @@
-#ifndef RTC_NIGHT_ALARM_H
-#define RTC_NIGHT_ALARM_H
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
-#include "config.h"
+#include <Arduino.h>
+#include <RTClib.h>
 
-bool rtc_isNightTime();
-void rtc_nightSleepTo5am();
+struct SunTimes {
+    uint8_t sunrise_h;
+    uint8_t sunrise_m;
+    uint8_t sunset_h;
+    uint8_t sunset_m;
+};
 
-#endif
+extern RTC_DATA_ATTR uint8_t scheduler_night_sleep_flag;
+
+SunTimes scheduler_get_for_date(uint8_t month, uint8_t day);
+
+SunTimes scheduler_get(uint8_t month);
+
+void scheduler_is_daytime();
+
+void scheduler_print_daytime_window();
+
+void scheduler_check_and_sleep_if_night();
+
+#endif 
